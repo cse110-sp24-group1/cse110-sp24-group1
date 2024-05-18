c7a449b9f30effa1a2604d48444572b9d34fa193
Katie Kim <kak010@ucsd.edu>
2024-05-18 14:50:33 -0700

    Ran and implemented standard npx to fix the home.js file

--- src/assets/scripts/home.js
+++ src/assets/scripts/home.js
@@ -1,27 +1,27 @@
 class HomeScript {
-    constructor() {
-        // Selects the new note button
-        this.newNoteButton = document.getElementById('new-note-button');
-        // Selects the top right buttons: new note and new folder
-        this.topRightButtons = document.querySelector('.top-right-buttons');
-        // Selects the navigation bar
-        this.navBar = document.querySelector('nav');
+  constructor () {
+    // Selects the new note button
+    this.newNoteButton = document.getElementById('new-note-button')
+    // Selects the top right buttons: new note and new folder
+    this.topRightButtons = document.querySelector('.top-right-buttons')
+    // Selects the navigation bar
+    this.navBar = document.querySelector('nav')
 
-        // Add event listener to open the modal on click of the new note button
-        this.newNoteButton.addEventListener('click', this.openModal.bind(this));
-        
-    }
-    // Opens the modal to the new note button
-    openModal() {
-        // Add blur class to navigation bar
-        this.navBar.classList.add('blur');
-        // Create modal element for 'div' of home html
-        const modal = document.createElement('div');
-        // Modal class for css design 
-        modal.classList.add('modal');
+    // Add event listener to open the modal on click of the new note button
+    this.newNoteButton.addEventListener('click', this.openModal.bind(this))
+  }
 
-        // Modal content
-        modal.innerHTML = `
+  // Opens the modal to the new note button
+  openModal () {
+    // Add blur class to navigation bar
+    this.navBar.classList.add('blur')
+    // Create modal element for 'div' of home html
+    const modal = document.createElement('div')
+    // Modal class for css design
+    modal.classList.add('modal')
+
+    // Modal content
+    modal.innerHTML = `
             <div class='note-modal'>
                 <span class='close-modal'>&times;</span>
                 <div class='modal-title'>
@@ -43,24 +43,23 @@ class HomeScript {
                 </form>
             </div>
         `
-        // Append modal to main body 
-        document.body.appendChild(modal);
-        // Hide the top right buttons
-        this.topRightButtons.style.display = 'none';
+    // Append modal to main body
+    document.body.appendChild(modal)
+    // Hide the top right buttons
+    this.topRightButtons.style.display = 'none'
 
-        // Close modal when clicking the close button
-        const closeButton = modal.querySelector('.close-modal');
-        closeButton.addEventListener('click', () => {
-            document.body.removeChild(modal);
-            // Show the top right buttons again
-            this.topRightButtons.style.display = 'flex';
-            // Remove the blur class from the navigation bar
-            this.navBar.classList.remove('blur');
-        });
-        const createButton = modal.querySelector('#saveNoteButton');
-
-    }
+    // Close modal when clicking the close button
+    const closeButton = modal.querySelector('.close-modal')
+    closeButton.addEventListener('click', () => {
+      document.body.removeChild(modal)
+      // Show the top right buttons again
+      this.topRightButtons.style.display = 'flex'
+      // Remove the blur class from the navigation bar
+      this.navBar.classList.remove('blur')
+    })
+    const createButton = modal.querySelector('#saveNoteButton')
+  }
 }
 document.addEventListener('DOMContentLoaded', () => {
-    new HomeScript();
-});
+  new HomeScript()
+})
