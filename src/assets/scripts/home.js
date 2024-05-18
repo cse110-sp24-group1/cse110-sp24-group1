299dfbecaf36151966c05317a52028025871f4ed
Pongsatorn Chanpanichravee <pongsatorn1612@berkeley.edu>
2024-05-18 16:20:45 -0700

    Create  the exist note module. For now, Assume the button to be the exist note

--- src/assets/scripts/home.js
+++ src/assets/scripts/home.js
@@ -6,9 +6,14 @@ class HomeScript {
         this.topRightButtons = document.querySelector('.top-right-buttons');
         // Selects the navigation bar
         this.navBar = document.querySelector('nav');
+        // Selects the exist note
+        this.existNoteButton = document.getElementById('exist-note-button');
 
         // Add event listener to open the modal on click of the new note button
         this.newNoteButton.addEventListener('click', this.openModal.bind(this));
+
+        // Add event listener to open the modal on click of the existing note button
+        this.existNoteButton.addEventListener('click', this.editModal.bind(this));
         
     }
     // Opens the modal to the new note button
@@ -58,6 +63,48 @@ class HomeScript {
             this.navBar.classList.remove('blur');
         });
         const createButton = modal.querySelector('#saveNoteButton');
+    }
+
+
+    // Opens the modal to the exist note
+    editModal() {
+        // Add blur class to navigation bar
+        this.navBar.classList.add('blur');
+        // Create modal element for 'div' of home html
+        const modal = document.createElement('div');
+        // Modal class for css design 
+        modal.classList.add('modal');
+
+        // Modal content
+        modal.innerHTML = `
+            <div class='note-modal'>
+                <div class='modal-title'>
+                    <h2>Note Title</h2>
+                </div>
+                <form id='note-modal-form'>
+                    <button class='back-button' type='submit'>Back</button>
+                    <div class='modal-input'>
+                        <textarea id='note-body' name='note-body'></textarea>
+                    </div>
+                    <button class='save-button' type='submit'>Save</button>
+                </form>
+            </div>
+        `
+        // Append modal to main body 
+        document.body.appendChild(modal);
+        // Hide the top right buttons
+        this.topRightButtons.style.display = 'none';
+
+        // Close modal when clicking the back button
+        const backButton = modal.querySelector('.back-button');
+        backButton.addEventListener('click', () => {
+            document.body.removeChild(modal);
+            // Show the top right buttons again
+            this.topRightButtons.style.display = 'flex';
+            // Remove the blur class from the navigation bar
+            this.navBar.classList.remove('blur');
+        });
+        //const createButton = modal.querySelector('#saveNoteButton');
 
     }
 }
