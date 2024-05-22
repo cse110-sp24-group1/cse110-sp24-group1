a858aa6638af110769b079ee54e820ee015af6ac
anshisinghh <anshisingh730@gmail.com>
2024-05-22 13:39:56 -0700

    fixed styling issues, made them consistent, added dark/light mode

---
+++ src/scripts/global.js
@@ -0,0 +1,33 @@
+const darkModeToggle = document.getElementById("darkModeToggle");
+
+// Check the user's preferred color scheme
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+// Set the initial theme based on localStorage or user's preferred color scheme
+let currentTheme = localStorage.getItem("theme");
+if (!currentTheme) {
+    currentTheme = prefersDarkScheme.matches ? "dark" : "light";
+    localStorage.setItem("theme", currentTheme);
+}
+
+// Apply the initial theme
+if (currentTheme === "dark") {
+    document.body.classList.add("dark-mode");
+    darkModeToggle.checked = true;
+} else {
+    document.body.classList.add("light-mode");
+    darkModeToggle.checked = false;
+}
+
+// Toggle the theme when the toggle switch is clicked
+darkModeToggle.addEventListener("change", function () {
+    if (darkModeToggle.checked) {
+        document.body.classList.remove("light-mode");
+        document.body.classList.add("dark-mode");
+        localStorage.setItem("theme", "dark");
+    } else {
+        document.body.classList.remove("dark-mode");
+        document.body.classList.add("light-mode");
+        localStorage.setItem("theme", "light");
+    }
+});
